7697f2fe81a0998cc768cd24c31e36ad34789501
Andrews Medina <andrewsmedina@gmail.com>
2019-12-08 16:56:13 -0300

    add linked

---
+++ linked-list/linkedList.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct node {
+	struct node *next;
+	int value;
+};
+
+int main() {
+	struct node n;
+	for (int i=0; i<10; i++) {
+		:q	
+	}
+	n.value = 0;
+	printf("%d", n.value);
+	return 0;
+}
